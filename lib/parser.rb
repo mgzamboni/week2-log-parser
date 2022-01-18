ad04369c76a1d7da90f4bf9f6deafabba7856646
mgzamboni <matheusgzamboni@gmail.com>
2022-01-18 15:54:07 -0300

    fix indentation to two spaces

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,15 +1,15 @@
 class Parser
-    attr_accessor :file_pathname
+  attr_accessor :file_pathname
 
-    def initialize(file_pathname)
-        @file_pathname = file_pathname
-    end
+  def initialize(file_pathname)
+    @file_pathname = file_pathname
+  end
 
-    def get_first_line
-        if(File.file?(@file_pathname))
-            first_line = File.foreach(@file_pathname).first(1).join(", ")
-        else
-            raise "file or directory not found"
-        end
+  def get_first_line
+    if(File.file?(@file_pathname))
+      first_line = File.foreach(@file_pathname).first(1).join(", ")
+    else
+      raise "file or directory not found"
     end
+  end
 end
