396548ef9784c573894db5b7249698773c833956
mgzamboni <matheusgzamboni@gmail.com>
2022-01-18 16:50:27 -0300

    changed doubles quotes to single quotes since there's no string interpolation

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,19 +1,19 @@
-require "parser.rb"
+require 'parser.rb'
 
-describe "Parser" do
-  it "reads the first line of the games.log file" do
-    fl = Parser.new("logs/games.log")
+describe 'Parser' do
+  it 'reads the first line of the games.log file' do
+    fl = Parser.new('logs/games.log')
     expect(fl.get_first_line).to be_a(String)
   end
 
-  it "checks if the file exists" do
-    fl = Parser.new("invalid_directory/invalid_file.txt")
+  it 'checks if the file exists' do
+    fl = Parser.new('invalid_directory/invalid_file.txt')
     expect{fl.get_first_line}.to raise_error(RuntimeError, 'file or directory not found')
   end
 
-  it "checks if reading from valid file" do
-    fl = Parser.new("logs/games.log").get_first_line
-    expect(fl).to eql("  0:00 ------------------------------------------------------------")
+  it 'checks if reading from valid file' do
+    fl = Parser.new('logs/games.log').get_first_line
+    expect(fl).to eql("  0:00 ------------------------------------------------------------\n")
   end
 end
   
