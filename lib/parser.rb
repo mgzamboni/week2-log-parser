3cb68f3cb7c8febcc28fb6bd46b877a1d00219df
mgzamboni <matheusgzamboni@gmail.com>
2022-01-18 15:57:16 -0300

    remove unecessary '()' from if statement

--- lib/parser.rb
+++ lib/parser.rb
@@ -6,7 +6,7 @@ class Parser
   end
 
   def get_first_line
-    if(File.file?(@file_pathname))
+    if File.file?(@file_pathname)
       first_line = File.foreach(@file_pathname).first(1).join(", ")
     else
       raise "file or directory not found"
