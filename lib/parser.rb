82d62307522861deea3432a106e29af920ebffc8
mgzamboni <matheusgzamboni@gmail.com>
2022-01-19 11:41:36 -0300

    update get_nlines to private method

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Parser
   attr_accessor :file_pathname
 
@@ -9,14 +11,14 @@ class Parser
     first_line = File.foreach(file_pathname).first unless validate_file
   end
 
-  def get_nlines
-    first_line = File.foreach(file_pathname).inject(0) {|count, line| count+1} unless validate_file
-  end
-
   private
 
   def validate_file
     raise 'file or directory not found' unless File.file?(file_pathname)
   end
+
+  def get_nlines
+    first_line = File.foreach(file_pathname).inject(0) {|count, line| count+1} unless validate_file
+  end 
 
 end
