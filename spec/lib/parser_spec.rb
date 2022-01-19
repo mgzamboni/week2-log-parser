28f19d17774fdf250c7e328257a91c2f3c24ca24
mgzamboni <matheusgzamboni@gmail.com>
2022-01-19 08:43:24 -0300

    fix pathfile to correct one

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -2,7 +2,7 @@ require 'parser.rb'
 
 describe 'Parser' do
   it 'reads the first line of the games.log file' do
-    fl = Parser.new('spec/fixtures/games_test.log')
+    fl = Parser.new('spec/fixtures/game_test.log')
     expect(fl.get_first_line).to be_a(String)
   end
 
@@ -12,7 +12,7 @@ describe 'Parser' do
   end
 
   it 'checks if reading from valid file' do
-    fl = Parser.new('spec/fixtures/games_test.log').get_first_line
+    fl = Parser.new('spec/fixtures/game_test.log').get_first_line
     expect(fl).to eql("  0:00 ------------------------------------------------------------\n")
   end
 end
