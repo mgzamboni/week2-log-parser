b6275c3ce073f9b63bd542911e51507d9dad4652
mgzamboni <matheusgzamboni@gmail.com>
2022-01-20 16:25:02 -0300

    update method call to get_gamelog_json

--- main.rb
+++ main.rb
@@ -4,4 +4,4 @@ require "json"
 file = Parser.new('spec/fixtures/game_test.log')
 
 puts "> get_first_line method return:\n#{file.get_first_line}\n"
-puts "> get_nlines_json method return:\n#{JSON.pretty_generate(JSON.parse(file.get_nlines_json))}\n"
+puts "> get_nlines_json method return:\n#{JSON.pretty_generate(JSON.parse(file.get_gamelog_json))}\n"
