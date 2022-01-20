7a50877a844bdd9ec4041bfea7bf0c36acf45c0b
mgzamboni <matheusgzamboni@gmail.com>
2022-01-20 12:21:31 -0300

    update rspec tests following Better Specs guidelines

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,23 +1,37 @@
 require 'parser.rb'
 
-describe 'Parser' do
-  it 'reads the first line of the games.log file' do
-    fl = Parser.new('spec/fixtures/game_test.log')
-    expect(fl.get_first_line).to be_a(String)
+describe Parser do
+  let(:valid_file) { Parser.new 'spec/fixtures/game_test.log' }
+  let(:invalid_file) {Parser.new 'invalid_folder/invalid_file.txt'}
+
+  describe '#get_first_line' do
+    context 'when valid file' do
+      it 'checks if it returns a valid string' do
+        expect(valid_file.get_first_line).to eql("  0:00 ------------------------------------------------------------\n")
+      end
+    end
+    
+    context 'when invalid file' do
+      it 'checks if it returns a runtime error' do
+        expect{invalid_file.get_first_line}.to raise_error(RuntimeError, 'file or directory not found')
+      end
+    end
+
   end
 
-  it 'checks if the file exists' do
-    fl = Parser.new('invalid_directory/invalid_file.txt')
-    expect{fl.get_first_line}.to raise_error(RuntimeError, 'file or directory not found')
-  end
+  describe '#get_nlines_json' do
 
-  it 'checks if reading from valid file' do
-    fl = Parser.new('spec/fixtures/game_test.log').get_first_line
-    expect(fl).to eql("  0:00 ------------------------------------------------------------\n")
-  end
+    context 'when valid file' do
+      it 'checks if it returns a valid json with correrct info' do
+        expect(valid_file.get_nlines_json).to eql('{"game_test.log":{"lines":10}}')
+      end
+    end
 
-  it 'checks if it returns a valid json file with the right info' do
-    fl = Parser.new('spec/fixtures/game_test.log').get_nlines_json
-    expect(fl).to eql('{"game_test.log":{"lines":10}}')
+    context 'when invalid file' do
+      it 'checks if it returns a runtime error' do
+        expect{invalid_file.get_nlines_json}.to raise_error(RuntimeError, 'file or directory not found')
+      end
+    end
+
   end
 end
