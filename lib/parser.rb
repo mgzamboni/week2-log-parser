b607f80bf6b3b5f74345eaad252bc8f2b83e9e77
mgzamboni <matheusgzamboni@gmail.com>
2022-01-21 18:21:43 -0300

    remove method filter_player_names and add methods to filter data separately

--- lib/parser.rb
+++ lib/parser.rb
@@ -25,32 +25,29 @@ class Parser
     players_arr = []
     count = 0
     File.foreach(file_pathname) do |line|
-      filter_player_names(line.split(' '), players_arr)
+      kill_data(line.split(' '), players_arr)
+      infochange_data(line.split(' '), players_arr)
       count += 1
     end
     [count, players_arr]
   end
 
   def kill_data(log_line, players_arr)
-    killed_index = log_line.index 'killed'
-    by_index = log_line.index 'by'
-    player_killer = log_line[5..killed_index - 1].join(' ')
-    player_killed = log_line[killed_index + 1..by_index - 1].join(' ')
-    unless players_arr.include? player_killer
-      players_arr << player_killer unless player_killer == '<world>'
+    unless log_line[1] != 'Kill:'
+      killed_index = log_line.index 'killed'
+      by_index = log_line.index 'by'
+      player_killer = log_line[5..killed_index - 1].join(' ')
+      player_killed = log_line[killed_index + 1..by_index - 1].join(' ')
+      unless players_arr.include? player_killer
+        players_arr << player_killer unless player_killer == '<world>'
+      end
+      players_arr << player_killed unless players_arr.include? player_killed
     end
-    players_arr << player_killed unless players_arr.include? player_killed
   end
 
-  def filter_player_names(log_line, players_arr)
-    case log_line[1]
-    when 'Kill:'
-      kill_data(log_line, players_arr)
-
-    when 'ClientUserinfoChanged:'
-      user_name = log_line[3].split('\\')[1]
-      players_arr << user_name unless players_arr.include? user_name
+  def infochange_data(log_line, players_arr)
+    unless log_line[1] != 'ClientUserinfoChanged:'
+      players_arr << log_line[3].split('\\')[1] unless players_arr.include? log_line[3].split('\\')[1]
     end
-    players_arr
   end
 end
