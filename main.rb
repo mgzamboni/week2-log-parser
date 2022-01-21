60baf03eb1bcd39f875667962e8370c157581ad7
mgzamboni <matheusgzamboni@gmail.com>
2022-01-21 17:55:35 -0300

    fix to correct methods names

--- main.rb
+++ main.rb
@@ -3,5 +3,5 @@ require 'json'
 
 file = Parser.new('spec/fixtures/game_test.log')
 
-puts "> get_first_line method return:\n#{file.first_line}\n"
-puts "> get_nlines_json method return:\n#{JSON.pretty_generate(JSON.parse(file.gamelog_json))}\n"
+puts "> first_line method return:\n#{file.first_line}\n"
+puts "> gamelog_json method return:\n#{JSON.pretty_generate(JSON.parse(file.gamelog_json))}\n"
